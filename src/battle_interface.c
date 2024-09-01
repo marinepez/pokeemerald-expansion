55c87474ab6d4515639efeb574e690e158679fd6
marinepez <95157120+marinepez@users.noreply.github.com>
2024-09-01 12:51:19 -0700

    Removed level display for opponent

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -2582,8 +2582,7 @@ void UpdateHealthboxAttribute(u8 healthboxSpriteId, struct Pokemon *mon, u8 elem
     }
     else
     {
-        if (elementId == HEALTHBOX_LEVEL || elementId == HEALTHBOX_ALL)
-            UpdateLvlInHealthbox(healthboxSpriteId, GetMonData(mon, MON_DATA_LEVEL));
+        //Never display opposing mon's level
         if (gBattleSpritesDataPtr->battlerData[battlerId].hpNumbersNoBars)
         {
             if (elementId == HEALTHBOX_ALL)
